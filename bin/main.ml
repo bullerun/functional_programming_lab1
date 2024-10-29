a8bd0ab6291f48553498d8e2df889fafd6776074
bullerun <ryskalwork@mail.ru>
2024-10-29 05:49:49 +0300

    Task 2 is done

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,21 @@
-let () = print_endline "Hello, World!"
-let () = print_int 12
+open Task2
 
+let () =
+  print_int recursion;
+  print_newline ()
 
+let () =
+  print_int tail_recursion;
+  print_newline ()
 
+let () =
+  print_int module_fibonacci;
+  print_newline ()
 
+let () =
+  print_int while_fibonacci;
+  print_newline ()
 
+let () =
+  print_int infinity_list;
+  print_newline ()
---
+++ bin/task2/task2.ml
@@ -0,0 +1,44 @@
+let recursion =
+  let rec fibonacci a b =
+    if a > 4_000_000 then 0
+    else
+      let summ = fibonacci b (a + b) in
+      if a mod 2 = 0 then a + summ else summ
+  in
+  fibonacci 0 1
+
+let tail_recursion =
+  let rec fibonacci a b summ =
+    if a > 4_000_000 then summ
+    else
+      let summ = if a mod 2 = 0 then summ + a else summ in
+      fibonacci b (a + b) summ
+  in
+  fibonacci 0 1 0
+
+let module_fibonacci =
+  let rec aux a b acc =
+    if a > 4_000_000 then acc else aux b (a + b) (a :: acc)
+  in
+  aux 0 1 [] |> List.filter (fun x -> x mod 2 = 0) |> List.fold_left ( + ) 0
+
+let while_fibonacci =
+  let a = ref 0 in
+  let b = ref 1 in
+  let ans = ref 0 in
+  while !a <= 4_000_000 do
+    if !a mod 2 = 0 then ans := !ans + !a;
+    let tmp = !a in
+    a := !a + !b;
+    b := tmp
+  done;
+  !ans
+
+let infinity_list =
+  let fibonacci_sequence =
+    Seq.unfold (fun (a, b) -> Some (a, (b, a + b))) (0, 1)
+  in
+  fibonacci_sequence
+  |> Seq.take_while (fun x -> x <= 4_000_000)
+  |> Seq.filter (fun x -> x mod 2 = 0)
+  |> Seq.fold_left ( + ) 0
